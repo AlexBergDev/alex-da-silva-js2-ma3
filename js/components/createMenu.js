1e507b0ed5df17f642fc40b658335ce96635407f
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-10-07 16:21:47 +0200

    Update createMenu.js

--- js/components/createMenu.js
+++ js/components/createMenu.js
@@ -12,13 +12,13 @@ export default function createMenu() {
 
     if (username) {
         authLink = `<div class="mt-1 mx-3 text-muted">Logged in: <strong class="text-dark text-capitalize">${username}</strong></div>
-                        <a class="btn btn-warning" href="profile.html">Profile</a>
+                        <a class="btn btn-warning ${pathname === "/profile.html" ? "active" : ""}" href="profile.html">Profile</a>
                         <button class="btn btn-danger" id="logout">Logout</button>`;
     }
 
     container.innerHTML = `<nav class="navbar navbar-expand-sm navbar-light bg-light">
                             <div class="container">
-                                <a class="navbar-brand ${pathname === "/" ? "active" : ""}" href="/">JS2 | M2</a>
+                                <a class="navbar-brand" href="/">JS2 | M2</a>
                                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span class="navbar-toggler-icon"></span>
                                 </button>

200a7bc9252fe784fbe30436ebad6692a96cdb77
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-10-07 17:49:18 +0200

    Update createMenu.js

--- js/components/createMenu.js
+++ js/components/createMenu.js
@@ -4,7 +4,7 @@ import logoutButton from "./logoutButton.js";
 export default function createMenu() {
     const { pathname } = document.location;
 
-    const container = document.querySelector(".menu-container");
+    const container = document.querySelector(".navigation");
 
     const username = getUsername();
 
@@ -12,14 +12,16 @@ export default function createMenu() {
 
     if (username) {
         authLink = `<div class="mt-1 mx-3 text-muted">Logged in: <strong class="text-dark text-capitalize">${username}</strong></div>
-                        <a class="btn btn-warning ${pathname === "/profile.html" ? "active" : ""}" href="profile.html">Profile</a>
-                        <button class="btn btn-danger" id="logout">Logout</button>`;
+                    <div class="row">
+                        <a class="col-sm btn btn-warning ${pathname === "/profile.html" ? "active" : ""}" href="profile.html">Profile</a>
+                        <button class="col-sm btn btn-danger" id="logout">Logout</button>
+                    </div>`;
     }
 
     container.innerHTML = `<nav class="navbar navbar-expand-sm navbar-light bg-light">
                             <div class="container">
                                 <a class="navbar-brand" href="/">JS2 | M2</a>
-                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <button class="navbar-toggler border-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span class="navbar-toggler-icon"></span>
                                 </button>
                                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
